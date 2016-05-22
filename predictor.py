40227a07c4cc23fc6832acdd08168a96d77cf3e2
rnov <rsmarinov@gmail.com>
2016-05-22 16:11:37 +0100

    falta unir con rails

--- predictor.py
+++ predictor.py
@@ -1,40 +1,34 @@
-  # coding=<utf-8>
 from flask import Flask
-from flask import request
-from pandas import DataFrame
-from sklearn.externals import joblib
-import numpy
 app = Flask(__name__)
+from sklearn.externals import joblib
 
-
-
-
-  # Cargo el modelo que he entrenado previamente.
-regr = joblib.load('operations.pkl')
+# load the chosen and trained model
+clf = joblib.load('operations.pkl')
 
 
 @app.route("/")
 def hello():
     return "Hello World!"
 
-@app.route("/predict", methods=['GET'])
-def predict():
-  arr_pene  = DataFrame([[request.args.get('op1'), request.args.get('op'), request.args.get('op2')]])
-  # Aqui va la magia de Machine Learning
-  print arr_pene
-  arr_pene2 = numpy.reshape(arr_pene, (2, 1))
-  print arr_pene2
-  result = regr.predict(arr_pene2)
-  print ("chupame un pier:" + result[0])
-  print ("pene")
-  return str(regr.predict(arr_pene))
+
+@app.route("/predict/<float:v>/")
+def predict(v):
+    '''
+    regr.predict(array) returns a list, [0] gets the only value in the list.
+    :param v: numpy.ndarray, format of v train_data[0::, [0, 1, 2, 4]]
+    :return: str, Time prediction in seconds
+    '''
+    v = v.reshape(1, -1)  # no warnings
+    return str(clf.predict([v])[0])
 
 if __name__ == "__main__":
+    # 3 examples
+    print clf.predict([36., 4., 50., 344.])
+    print clf.predict([94., 4., 37., 524.])
+    print clf.predict([9., 1., 36., 45.])
+    #app.run()
 
-    app.run()
-
-# Desde Rails podeis llamarlo asi
-
+# example: call from from rails
 # def estimate
 #   self.estimation = HTTP.get("http://localhost:5000/predict/#{op1.to_f}/").to_s
 # end
--- train.py
+++ train.py
@@ -3,65 +3,53 @@ import pandas as pd
 from sklearn import linear_model
 from sklearn.externals import joblib
 from sklearn import cross_validation
-from sklearn import svm
-import numpy as np
+
 
 # read data from file. csv
 train_df = pd.read_csv('allData.csv', header=0)     # Load the train file into a dataframe
 
-# translate operations (+,-,/,*) from str to int
-train_df['op'] = train_df['op'].map({'+': 0, '-': 0, '/': 1, '*': 2}).astype(int)
-
-#train_df['complexity'] = train_df['complexity'].map(len(train_df['op1'])>1 and len(train_df['op2'])> 1 : 15 ) #0 #por defecto complejidad 0 para todos
-
-
 # 'nombre' is useless for learning
 train_df = train_df.drop(['nombre'], axis=1)
-train_df['complexity'] = 0
 
-for c in range(0, len(train_df)):
-    if train_df.op[c] == 2:  #mul
-        if train_df.op1[c] > 9 and train_df.op2[c] > 9:
-            train_df['complexity'][c] = 3
-        else:
-            train_df['complexity'][c] = 2
-    elif train_df.op[c] == 1:  #div
-        if train_df.op1[c] > train_df.op2[c]:
-            train_df['complexity'] = 1
+# translate operations (+,-,/,*) from str to int
+train_df['op'] = train_df['op'].map({'+': 1, '-': 1, '/': 1, '*': 4}).astype(int)
 
-    elif train_df.op[c] == 0 and train_df.op1[c] > 9 and train_df.op2[c] > 9:  #suma y resta
-        train_df['complexity'][c] = 1
+# add new feature that measures the operation weight
+train_df['complexity'] = (train_df['op1'] + train_df['op2']) * train_df['op']
+
 # get the values
 train_data = train_df.values
-# print train_data[0::, :3:]  # from line 0 to end, gets all columns until 3th one
-# print train_data[0::, 3]  # from line 0 to end, gets just the 3th column
-train_data = np.roll(train_data, 2) #si se meten mas varibles aumentar este numero
 
-print 'Training...'
-# using linear model (regression, ridge, with multiple variables)for learning, since target are numbers (continuous)
-#clf = linear_model.Ridge(alpha=.5)
+#print train_data[0]
+#print train_data[0::, [0, 1, 2, 4]][0]
+#print train_data[0:, 3][0]
+
+
+print 'Training cross validation'
+# using linear model for learning, since target are numbers (continuous)
+
+#clf = linear_model.Ridge(alpha=1)
 clf = linear_model.LinearRegression()
-#data[:, [1, 9]] selecciona las columnas 1 y 9
-#clf.fit(train_data[0::, :3:], train_data[0::, 3])
-
-scores = cross_validation.cross_val_score(clf, train_data[0::, 1::], train_data[0:, 0], scoring='mean_absolute_error', cv = 10)
+# apply cross validation
+scores = cross_validation.cross_val_score(clf, train_data[0::, [0, 1, 2, 4]], train_data[0:, 3], scoring='mean_absolute_error', cv=10)
 
 print scores
-print 'max score is: {}'.format(max(scores))
+print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
 print 'mean score is: {}'.format(scores.mean())
 print 'standard deviation score is: {}'.format(scores.std()*2)
 
-# save the trained model,
-#joblib.dump(clf, 'operations.pkl')
 
-"""
-# shows attributes's type
-print type(train_df['op'][2])
-print type(train_df['op1'][2])
-print type(train_df['op2'][2])
-print type(train_df['tiempo'][2])
-#print train_df
-"""
+# entrenar
+clf.fit(train_data[0::, [0, 1, 2, 4]], train_data[0:, 3])
+
+#print type(train_data)
+for i in range(0, 10):
+    k = train_data[0::, [0, 1, 2, 4]][i]
+    k = k.reshape(1, -1)
+    print 'for operation {0} ,prediction is : {1}'.format(k, clf.predict(k)[0])
+
+# save the model trained model
+joblib.dump(clf, 'operations.pkl')
 
 # sources: http://scikit-learn.org/stable/modules/linear_model.html
 # http://scikit-learn.org/stable/modules/cross_validation.html
